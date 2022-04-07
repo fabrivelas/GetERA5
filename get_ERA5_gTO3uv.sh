b58d9382156ad1a7017c1734311dde16fe39aa61
Tobias Kerzenmacher <kerzenmacher@kit.edu>
2022-04-08 00:07:32 +0200

    Combined the preliminary ERA5 1950-1978 with the modern 1979-now
    Unified ERA5 download utility with added support for the years 1979 to now.

--- get_ERA5_gTO3uv.sh
+++ get_ERA5_gTO3uv.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=single
 $SBATCH --mem=6000
 #SBATCH --ntasks-per-node=12
+#SBATCH --mail-user=tobias.kerzenmacher@kit.edu
 #SBATCH --constraint=LSDF
-#SBATCH --mail-user=px5501@kit.edu
 
 ##source /home/kit/imk-asf/px5501/pythonenv/bin/activate
 source /home/kit/imk-asf/px5501/.Venv/Edge/bin/activate
 
 parallel -j 10 python3 /home/kit/imk-asf/px5501/Git/GetERA5/get_ERA5_gTO3uv.py $year ::: 1978
 install -p -g imk-asf-o3as-lsdf /pfs/work7/workspace/scratch/px5501-Era5/era5_mm_go3tuv1978.nc  /lsdf/kit/imk-asf/projects/O3as/Ecmwf/Era5/
-cp /pfs/work7/workspace/scratch/px5501-Era5/era5_mm_go3tuv1978.nc  /lsdf/kit/imk-asf/projects/O3as/Ecmwf/Era5/
+#cp /pfs/work7/workspace/scratch/px5501-Era5/era5_mm_go3tuv1978.nc  /lsdf/kit/imk-asf/projects/O3as/Ecmwf/Era5/
 #rm /pfs/work7/workspace/scratch/px5501-Era5/era5_mm_go3tuv1978.nc 
